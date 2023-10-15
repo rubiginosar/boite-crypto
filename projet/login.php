197b54f495c7b1df24bb3b6bbb6003cca35915a2
rubiginosar <tibiguinesrine@gmail.com>
2023-10-15 19:54:47 +0200

    username
    should be filled in

--- projet/login.php
+++ projet/login.php
@@ -1,3 +1,44 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Database connection setup (replace with your own database connection code)
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "boite_crypto";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        // User exists, so redirect to the home page
+        $_SESSION['authenticatedUsername'] = $username;
+        header("Location: home.php");
+        exit();
+    } else {
+        // User doesn't exist, redirect to the registration page or show an error message
+        echo "Please register or check your login credentials.";
+        exit();
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,46 +62,3 @@
     </div>
 </body>
 </html>
-
-<?php
-session_start();
-
-
-$authenticatedUsername = $_POST["username"]; // Replace with the username
-
-// Store user information in the session
-$_SESSION['authenticatedUsername'] = $authenticatedUsername;
-
-// Database connection setup (replace with your own database connection code)
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "boite_crypto";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    // Check if the user exists in the database
-    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // User exists, so redirect to the home page
-        header("Location: home.php");
-        exit();
-    } else {
-        // User doesn't exist, redirect to the registration page
-        echo "please register";
-        exit();
-    }
-}
-
-$conn->close();
-?>
